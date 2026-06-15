20f3be70f6f43c4f4dacfc76e372287153fae403
Julio-Cuellar <anyaquimera1@gmail.com>
2026-06-14 19:17:30 -0600

    chore: untrack target folders and update gitignore

--- medicloud/medicloud-app/target/classes/db/migration/V3__Fix_Inet_Columns_To_Varchar.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE core.refresh_tokens
-    ALTER COLUMN ip_address TYPE VARCHAR(45) USING ip_address::VARCHAR;
-
-ALTER TABLE core.password_reset_tokens
-    ALTER COLUMN requested_ip TYPE VARCHAR(45) USING requested_ip::VARCHAR;
